6510974ce4bfed89302799677d3abf7dcdf45f93
Eikty <mckozar@seznam.cz>
2025-11-30 17:03:59 +0100

    Bezier Curve and Spline

--- Translation/ICameraObserver.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class ICameraObserver {
-public:
-	virtual void update() = 0;
-};
--- Translation/ITransformation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glm/gtc/matrix_transform.hpp>
-
-class ITransformation {
-public:
-	virtual void addTransformation(ITransformation* transformation) {};
-	virtual glm::mat4 transform() = 0;
-};
